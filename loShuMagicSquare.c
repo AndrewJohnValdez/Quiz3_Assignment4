f60964761ac3b345259571286711bc7b6af11a61
AndrewJohnValdez <andrewjohnv@cpp.edu>
2022-10-22 13:32:06 -0700

    Magic Square outline

---
+++ loShuMagicSquare.c
@@ -0,0 +1,59 @@
+/*
+Lo Shu Magic Square
+
+int[][] array
+
+Main is defined in main, no input from user needed
+
+contains numbers 1 - 9 exactly
+NO NUMBER REPEATED
+each row, column, and diagonal must add up to the same number
+2d array
+function that accepts a 2d array as arg and determine if the array is a lo shu magic square
+put random number placement into loop until array is a lo shu magic square
+
+Print results:
+    total number of squares generated and tested before success (recording by counting the loop)
+    Print the final square and its placements
+    [a b c]
+    [d e f]
+    [g h i]
+
+*/
+
+/*
+int check() 
+{
+    //check the values in the diagonal, row, and column areas
+
+    //if statements regarding the 3 requirements and test to see if they all equal same value
+}
+
+
+int main() 
+{
+    //Global variables
+    //int count;
+    //int[][] square;
+
+    //declare two [][]arrays
+        //one that is in magic square form
+        //one in incorrect form
+    
+    //function to create square (a generator that creates based on size)
+    //3x3 = 9 size, sum in each row = 15
+    //for loop with if-statements to position numbers without reusing
+
+
+    //function to test square
+    //checks the values of the array and returns true of false
+
+    //call function to test 
+    //cycles the create and test function until a correct magic square is formed
+    //record number of loops done
+
+
+    return EXIT_SUCCESS;
+}
+
+*/
